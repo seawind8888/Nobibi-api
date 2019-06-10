ac0c941259968418704f85a474c1be283e4341ba
heaven.wang1 <heaven.wang@yeahmobi.com>
2019-06-10 20:00:03 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ const express = require('express');
 const router = express.Router();
 
 import UserController from '../lib/controller/userController'
-import ArticleController from '../lib/controller/articleController';
+import TopicController from '../lib/controller/topicController';
 import MenuController from '../lib/controller/menuController'
 import CategorytController from '../lib/controller/categorytController'
 /**
  * 用户管理
  */
-router.post('/user/create',UserController.userAddAction);
+router.post('/user/createUser',UserController.userAddAction);
 router.post('/user/login',UserController.userLogin);
 router.get('/user/logout',UserController.userLogout);
-router.get('/user/info',UserController.userInfo);
-router.get('/user/list',UserController.userFindAction);
-router.post('/user/remove',UserController.userRemoveAction);
-router.patch('/user/update',UserController.userUpdateAction);
+router.get('/user/getUserInfo',UserController.userInfo);
+router.get('/user/getUserList',UserController.userFindAction);
+router.post('/user/removeUser',UserController.userRemoveAction);
+router.patch('/user/updateUser',UserController.userUpdateAction);
 
 /**
  * 文章管理
  */
-router.post('/article/create',ArticleController.articleAddAction)
-router.get('/article/list',ArticleController.articleFindAction);
-router.post('/article/remove',ArticleController.articleRemoveAction);
-router.post('/article/update',ArticleController.articleUpdateAction);
+router.post('/topic/createTopic',TopicController.topicAddAction)
+router.get('/topic/getTopicList',TopicController.topicFindAction);
+router.post('/topic/removeTopic',TopicController.topicRemoveAction);
+router.patch('/topic/updateTopic',TopicController.topicUpdateAction);
 
 /**
  * 菜单管理
@@ -36,10 +36,18 @@ router.post('/menu/update',MenuController.menuUpdateAction);
 /**
  * 分类管理
  */
-router.post('/category/create',CategorytController.categoryAddAction);
-router.get('/category/list',CategorytController.categoryFindAction);
-router.post('/category/remove',CategorytController.categoryRemoveAction);
-router.patch('/category/update',CategorytController.categoryUpdateAction);
+router.post('/category/createCategory',CategorytController.categoryAddAction);
+router.get('/category/getCategoryList',CategorytController.categoryFindAction);
+router.post('/category/removeCategory',CategorytController.categoryRemoveAction);
+router.patch('/category/updateCategory',CategorytController.categoryUpdateAction);
+
+// /**
+//  * 评论管理
+//  */
+// router.post('/comment/create',CommentController.commentAddAction);
+// router.get('/comment/list',CommentController.commentFindAction);
+// router.post('/comment/remove',CommentController.commentRemoveAction);
+// router.patch('/comment/update',CommentController.commentUpdateAction);
 
 
 
